15e0ed6c6cb5da70283ddb3d0729cf6b86ae0bde
Jeryl <jerylong1990@gmail.com>
2019-04-24 08:31:37 +0800

    modified config

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Olam@123!?'
-    SQLALCHEMY_DATABASE_URI = 'postgres://wwwbvuhuxuiecn:8cc3022620347c0f29ad84b0f43ab15f03db935008f65e1fdb4ac0eb4683e545@ec2-23-21-136-232.compute-1.amazonaws.com:5432/d3ionhq9q82n85'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://vwqcpgtjuxhbys:0294ea6b4bd9bb3b5ffa0f0398a3f7b398862c217cdf040aa3a75a0d133991be@ec2-54-243-241-62.compute-1.amazonaws.com:5432/dd5oob47f4rofr'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
