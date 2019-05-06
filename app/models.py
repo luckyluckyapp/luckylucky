ae0180c313230a5affe8d81f24a74fece3d0a072
Jeryl <jerylong1990@gmail.com>
2019-05-06 09:20:17 +0800

    add procfile

--- app/models.py
+++ app/models.py
@@ -1,10 +1,20 @@
 from app import db
 import datetime
+from wtforms_alchemy import ModelForm
+
+day = datetime.datetime.utcnow() + datetime.timedelta(hours=+8)
+todaysdate = day.strftime("%Y-%m-%d")
 
 class Orders(db.Model):
+    __tablename__ = 'orders'
     uid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=False)
-    numpacks = db.Column(db.String(64), index=True, unique=False)
+    numpack = db.Column(db.String(64), index=True, unique=False)
+    date = todaysdate
 
     def __repr__(self):
         return '<User {}>'.format(self.name)  
+
+class Orderform(ModelForm):
+    class Meta:
+        model = Orders
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,16 @@
 from flask import render_template, flash, redirect, request, Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
-from app import app
 from bs4 import BeautifulSoup
 import requests as req
 import re
 import datetime
-from app import models
-import os
+from app import app, models
 
 
 # if __name__ == '__main__':
 #     app.run()
 
-from app.forms import OrderForm
+from app.models import Orderform
 
 aaa = req.get("http://www.luckyluckycatering.com/lunch-menu")
 soup = BeautifulSoup(aaa.text,'html.parser')
@@ -24,6 +22,8 @@ fri =re.findall("Friday:[^.]*\.", aaa.text)[0]
 
 day = datetime.datetime.utcnow() + datetime.timedelta(hours=+8)
 wday = day.weekday()
+
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -49,12 +49,15 @@ def index(methods=['GET', 'POST']):
 	else:
 		dish1 = 'today no lucky lucky leh!'
 	
-	form = OrderForm()
-	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(form.name.data, form.numpacks.data))
-		return redirect(url_for('submitted'))
+	form = Orderform()
+	if request.method == 'POST':
+		user=form #load the model values
+		db.session.add(user) #gathers the session based data to be added in DB
+		db.session.commit() #Adds data to DB
+		flash('New order added!') #Display a message to end user at front end.
+		return redirect('/submitted') # redirects upon success to your homepage.
 	return render_template('index.html', dish=dish1, mon1=mon1, tue1=tue1, wed1=wed1, thur1=thur1, fri1=fri1, form=form)
-	
+
 # def add_user():
 #     user = OrderForm(request.form['name']), request.form['numpacks']
 #     db.session.add(user)
@@ -64,3 +67,17 @@ def index(methods=['GET', 'POST']):
 @app.route('/submitted')
 def submitted():
 	return render_template('submitted.html')
+
+	# try:
+	# 	if form_validate_on_submit():
+	# 		user=Orderform() #load the model values
+	# 		form.populate_obj(Orderform) #populates the form with respective values 
+	# 		db.session.add(form) #gathers the session based data to be added in DB
+	# 		db.session.commit() #Adds data to DB
+	# 		flash('New order added!') #Display a message to end user at front end.
+	# 		return redirect('/submitted') # redirects upon success to your homepage.
+	# except Exception as e:
+	# 	# logs the errors
+	# 	db.session.rollback()
+	# 	flash('There was a problem ordering. Please contact Jordan.')
+	# 	return render_template('index.html', dish=dish1, mon1=mon1, tue1=tue1, wed1=wed1, thur1=thur1, fri1=fri1, form=form)
